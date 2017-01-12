b67851c0c60147b5e132e1f744ad75bf3b8bc1a5
Eugen Istoc <eugenistoc@gmail.com>
2017-01-12 01:42:47 -0500

    Show rbya.org feed on main tab

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import {Http } from '@angular/http';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser } from 'ionic-native';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  feed = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    private http:Http,
+    public navCtrl: NavController) {}
 
+  ngOnInit() {
+    console.log("hello");
+
+    this.http.get('api/feed/main')
+            .subscribe(response =>{
+                this.feed = response.json();
+              })
   }
 
+  readMore(link:string){
+    let browser = new InAppBrowser(link, '_system');
+
+    browser.show();
+  }
 }
