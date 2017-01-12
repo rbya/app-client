1f7ba536cbf587c84f52b88d80ca8246dc3a2e08
Eugen Istoc <eugenistoc@gmail.com>
2017-01-12 01:43:21 -0500

    Remove unused import

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import {Http } from '@angular/http';
 import { NavController } from 'ionic-angular';
 import { InAppBrowser } from 'ionic-native';
