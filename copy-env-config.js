95a3982e56dee5ca2de21a3a33eb6a7703a55b3d
Eugen Istoc <eugenistoc@gmail.com>
2017-01-17 15:17:46 -0500

    Hotfix for managing multiple enviornments when running for the first time.

--- copy-env-config.js
+++ copy-env-config.js
@@ -1,11 +1,16 @@
 var fs = require('fs');
 
+// Create www folder if it doesn't exist 
+if (!fs.existsSync('www')) {
+  fs.mkdir('www');
+}
+
 for (var i = 0; i < process.argv.length; i++) {
-    if (process.argv[i] === '--env') {
-        if (process.argv[i + 1]) {
-            jsonConfigurationFile = process.argv[i + 1] + '.json';
-        }
+  if (process.argv[i] === '--env') {
+    if (process.argv[i + 1]) {
+      jsonConfigurationFile = process.argv[i + 1] + '.json';
     }
+  }
 }
 
 var rd = fs.createReadStream('src/environments/' + jsonConfigurationFile);
@@ -15,10 +20,10 @@ rd.on('error', handleError);
 wd.on('error', handleError);
 
 function handleError(err) {
-    rd.destroy();
-    wd.end();
-    console.log('There was an error while copying the environment configuration');
-    console.log(err);
+  rd.destroy();
+  wd.end();
+  console.log('There was an error while copying the environment configuration');
+  console.log(err);
 }
 
 rd.pipe(wd);
